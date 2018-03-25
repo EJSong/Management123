04f0038e53e07731847f97bd737825c134d1ba21
SongEunJeoung <dmswjd7262@naver.com>
2018-03-26 05:03:52 +0900

    Log test commit

--- app/src/main/java/com/example/dmswj/management123/UserAreaActivity.java
+++ app/src/main/java/com/example/dmswj/management123/UserAreaActivity.java
@@ -1,5 +1,6 @@
 package com.example.dmswj.management123;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -16,7 +17,15 @@ public class UserAreaActivity extends AppCompatActivity {
         final EditText etAge = (EditText) findViewById(R.id.etAge);
         final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
 
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age",-1);
 
+        String message = name+ "welcome to your user area";
+        welcomeMessage.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age +"");
 
     }
 }
