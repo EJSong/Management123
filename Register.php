be4b3f629d8491f3718ae34dd742e120bc8b149e
EJSong <dmswjd7262@naver.com>
2018-03-25 17:46:48 +0000

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,13 +1,13 @@
 <?php
-	$con = mysqli_connect("127.0.0.1", "root", "soft1115", "root");
+	$con = mysqli_connect("127.0.0.1", "root", "", "root");
 
-	$userID = $_POST["userID"];
-	$userPassword = $_POST["userPassword"];
-	$userName = $_POST["userName"];
-	$userAge = $_POST["userAge"];
+	$name = $_POST["name"];
+	$age = $_POST["age"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
 
 	$statement = mysqli_prepare($con, "INSERT INTO USER VALUES (?, ?, ?, ?)");
-	mysqli_stmt_bind_param($statement, "sssi", $userID, $userPassword, $userName, $userAge);
+	mysqli_stmt_bind_param($statement, "sssi", $name, $age, $username, $password);
 	mysqli_stmt_execute($statement);
 
 	$response = array();
